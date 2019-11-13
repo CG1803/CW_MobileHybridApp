844eff6ea8f77ae95127a936e858b05f1b6419da
CG1803 <55310416+CG1803@users.noreply.github.com>
2019-11-13 23:52:37 +0400

    Update firstname.php

--- www/firstname.php
+++ www/firstname.php
@@ -12,7 +12,7 @@
         ' ' this root has no password protection, hence empty
         my_db is db with which we want to connect
     */
-    $con = mysqli_connect('localhost','root','','mydb') or die ('unable to connect');
+    $con = mysqli_connect('localhost','','','mydb') or die ('unable to connect');
 
     $ID = $_POST['ID'];
     $pass = $_POST['pass'];
@@ -20,8 +20,8 @@
     $result = mysqli_query($con,$sql);
     if($result) {
         echo $ID." ".$pass;
-    } 
+    }
     else {
         echo "unable to insert data";
-    }	
+    }
 ?>
